df16c36b245c01ad22bec37fd09c507c50df2bd2
fabio guelfi <fabioguelfiunix@gmail.com>
2018-09-16 12:14:10 -0300

    Passando Propriedades para Componentes Filhos (Parte 2)

--- ex/family.jsx
+++ ex/family.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { childrenWithProps } from '../utills/react.util';
 
 export default props => (
     <div>
         <h1>Familia</h1>
-        { React.cloneElement(props.children, { ...props }) }
+        {
+           childrenWithProps(props)
+        }
     </div>
 )
---
+++ utills/react.util.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const childrenWithProps = (props) => {
+    return  React.Children.map(props.children,
+        child => React.cloneElement(child, { ...props }))
+}
+
+export { childrenWithProps };
