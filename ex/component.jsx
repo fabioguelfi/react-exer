32fc742faf85bbb1351dc258d31345c7fadcc5c5
fabio guelfi <fabioguelfiunix@gmail.com>
2018-09-16 10:39:43 -0300

    Primeiro Componente (Arrow Function)

--- ex/component.jsx
+++ ex/component.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 
-export default function() {
-    return <h1>Primeiro Component!</h1>
-}
+export default () => (
+    <div>
+        <h1>Primeiro Component!</h1>
+        <h2>Segundo</h2>
+    </div>
+)
